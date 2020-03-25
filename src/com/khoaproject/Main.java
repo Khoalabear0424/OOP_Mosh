3fb71547068120698e892bd48d194fdc5c13e1ad
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2020-03-25 14:32:32 -0700

    Added gitignore file

--- src/com/khoaproject/Main.java
+++ src/com/khoaproject/Main.java
@@ -3,6 +3,11 @@ package com.khoaproject;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    var textBox1 = new TextBox();
+	    var textBox2 = new TextBox();
+        textBox1.setText("Box 1");
+        textBox2.setText("Box 2");
+        System.out.println(textBox1.text);
+        System.out.println(textBox2.text);
     }
 }
